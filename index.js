29cdaddd68b1425ece69cef8b3ef9623c7b109f9
Ornit Bhonkar <ornit321@gmail.com>
2021-10-30 01:57:48 +0300

    All 7 milestones

--- index.js
+++ index.js
@@ -1,38 +1,68 @@
-/* function fibonacci(x) {
-  let fiboArray = [0, 1];
-  for (let i = 1; i <= x; i++) {
-    fiboArray[i + 1] = fiboArray[i] + fiboArray[i - 1];
-  }
-  let y = fiboArray[x];
-  return y;
-}
+let largerThan50 = document.getElementById("largerThan50");
+largerThan50.style.display = "none";
+let loading = document.getElementById("loading");
+loading.style.display = "none";
+const button = document.getElementById("is");
+const inputNumber = document.getElementById("inputNumber");
+const resultOutput = document.getElementById("resultOutput");
+const loading2 = document.getElementById("loading2");
+loading2.style.display = "block";
+const listOfResults = document.getElementById("listOfResults");
+const checkBox = document.getElementById("save");
+window.addEventListener("load", getFiboResults);
 
-function whenClicked() {
-  let x = parseInt(document.getElementById("inputNumber").value);
-  document.getElementById("result").textContent = fiboRecursion(x);
-}
-
-function fiboRecursion(x) {
-  if (x <= 1) {
+/* function fiboRecursion(x) {
+  if ((x = 1)) {
     return x;
+  } else if (x < 1) {
+    return "number can't be smaller than 1";
+  } else if (x > 50) {
+    return "Number can't be larger than 50";
   } else {
     let y = fiboRecursion(x - 1) + fiboRecursion(x - 2);
     return y;
   }
 } */
-/* 
-const { result } = require("underscore");
 
- */
-let largerThan50 = document.getElementById("largerThan50");
-largerThan50.style.display = "none";
-let loading = document.getElementById("loading");
-loading.style.display = "none";
-let button = document.getElementById("is");
-const inputNumber = document.getElementById("inputNumber");
-let resultOutput = document.getElementById("resultOutput");
-button.addEventListener("click", callFiboServer);
-let number = parseInt(inputNumber.value);
+function serverOrLocal() {
+  if (!checkBox.checked) {
+    let x = parseInt(document.getElementById("inputNumber").value);
+    function fibonacci(x) {
+      let fiboArray = [0, 1];
+      let y;
+      if (x >= 1 && x <= 50) {
+        for (let i = 1; i <= x; i++) {
+          fiboArray[i + 1] = fiboArray[i] + fiboArray[i - 1];
+          resultOutput.style.textDecoration = "underline";
+          resultOutput.style.fontWeight = "bold";
+          resultOutput.style.fontSize = "x-large";
+          resultOutput.style.color = "#000";
+          resultOutput.innerHTML = fiboArray[x];
+        }
+      } else if (x > 50) {
+        largerThan50.style.display = "block";
+        loading.style.display = "none";
+        inputNumber.style.borderColor = "#D9534F";
+        inputNumber.style.color = "#D9534F";
+        resultOutput.style.fontSize = "medium";
+        resultOutput.style.fontWeight = "normal";
+      } else if (x < 1) {
+        largerThan50.style.display = "none";
+        loading.style.display = "none";
+        inputNumber.style.borderColor = "#D9534F";
+        inputNumber.style.color = "#D9534F";
+        resultOutput.innerHTML = "number can't be smaller than 1";
+        resultOutput.style.color = "#D9534F";
+        resultOutput.style.fontSize = "medium";
+        resultOutput.style.textDecoration = "none";
+        resultOutput.style.fontWeight = "normal";
+      }
+    }
+    fibonacci(x);
+  } else {
+    callFiboServer();
+  }
+}
 
 function callFiboServer() {
   let number = parseInt(inputNumber.value);
@@ -59,26 +89,19 @@ function callFiboServer() {
         resultOutput.style.textDecoration = "underline";
         resultOutput.style.fontWeight = "bold";
         resultOutput.style.fontSize = "x-large";
+        resultOutput.style.color = "#000";
+        loading2.style.display = "block";
         getFiboResults();
       })
       .catch((error) => {
         loading.style.display = "none";
         resultOutput.innerHTML = "Server Error: " + error.message;
         resultOutput.style.color = "#D9534F";
+        resultOutput.style.textDecoration = "none";
+        resultOutput.style.fontSize = "medium";
       });
   }
 }
-
-function onClickField() {
-  inputNumber.style.color = "#000";
-  inputNumber.style.borderColor = "#CCCCCC";
-  largerThan50.style.display = "none";
-  resultOutput.innerHTML = "";
-  inputNumber.value = "";
-}
-
-const listOfResults = document.getElementById("listOfResults");
-const loading2 = document.getElementById("loading2");
 
 function getFiboResults() {
   let resultsURL = "http://localhost:5050/getFibonacciResults";
@@ -95,18 +118,17 @@ function getFiboResults() {
         let inputNumber = singleResult["number"];
         let resultOutput = singleResult["result"];
         let fullSentence = `The fibonacci of <b>${inputNumber}</b> is <b>${resultOutput}</b>. Calculated at: ${date}`;
-        const element1 = `<li class="resultSentence">${fullSentence}</li>`;
-        listOfResults.innerHTML += element1;
+        const element = `<li class="resultSentence">${fullSentence}</li>`;
+        listOfResults.innerHTML += element;
       }
       loading2.style.display = "none";
     });
 }
 
-window.addEventListener("load", getFiboResults);
-
-function serverOrLocal() {
-  let checkBox = document.getElementById("save");
-  if (checkBox.checked) {
-    return "Hi";
-  }
+function onClickField() {
+  inputNumber.style.color = "#000";
+  inputNumber.style.borderColor = "#CCCCCC";
+  largerThan50.style.display = "none";
+  resultOutput.innerHTML = "";
+  inputNumber.value = "";
 }
